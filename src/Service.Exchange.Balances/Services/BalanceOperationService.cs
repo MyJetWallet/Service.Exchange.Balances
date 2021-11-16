2b98e728ff7b4795af75a463334a5071afc3a9b6
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-11-17 00:28:55 +0200

    -  fix;

--- src/Service.Exchange.Balances/Services/BalanceOperationService.cs
+++ src/Service.Exchange.Balances/Services/BalanceOperationService.cs
@@ -2,8 +2,10 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using MyJetWallet.Sdk.Service;
+using MyJetWallet.Sdk.ServiceBus;
 using Newtonsoft.Json;
 using Service.Exchange.Balances.Domain;
+using Service.Exchange.Balances.Domain.Models.ServiceBus;
 using Service.Exchange.Balances.Grpc;
 using Service.Exchange.Sdk.Messages;
 
@@ -13,11 +15,14 @@ namespace Service.Exchange.Balances.Services
     {
         private readonly ILogger<BalanceOperationService> _logger;
         private readonly IBalancesService _balancesService;
+        private readonly IServiceBusPublisher<ExBalanceUpdateMessage> _depositPublisher;
 
-        public BalanceOperationService(ILogger<BalanceOperationService> logger, IBalancesService balancesService)
+        public BalanceOperationService(ILogger<BalanceOperationService> logger, IBalancesService balancesService,
+            IServiceBusPublisher<ExBalanceUpdateMessage> depositPublisher)
         {
             _logger = logger;
             _balancesService = balancesService;
+            _depositPublisher = depositPublisher;
         }
 
         public async Task<List<ExBalanceUpdate>> ProcessBalanceUpdates(List<ExBalanceUpdateInstruction> request)
@@ -32,6 +37,8 @@ namespace Service.Exchange.Balances.Services
             {
                 var responseResult = await _balancesService.ProcessBalanceUpdates(exBalanceUpdateInstruction);
                 result.Add(responseResult);
+
+                await _depositPublisher.PublishAsync(new ExBalanceUpdateMessage(responseResult));
             }
 
             result.AddToActivityAsJsonTag("response-data");
