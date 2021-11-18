9895e2ed55f44060526aae054e6d45680c8e93f3
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-11-18 12:52:13 +0200

    - fix precision;

--- src/Service.Exchange.Balances.Postgres/Migrations/20211118105153_UpdateBalancePrecision.Designer.cs
+++ src/Service.Exchange.Balances.Postgres/Migrations/20211118105153_UpdateBalancePrecision.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Service.Exchange.Balances.Postgres;
@@ -11,9 +12,10 @@ using Service.Exchange.Balances.Postgres;
 namespace Service.Exchange.Balances.Postgres.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20211118105153_UpdateBalancePrecision")]
+    partial class UpdateBalancePrecision
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
